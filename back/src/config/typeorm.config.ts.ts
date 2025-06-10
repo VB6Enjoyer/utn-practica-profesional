d58c3214121114f0b296132351567bb49a2dc9cd
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-06-09 22:47:35 -0300

    Update typeorm.config.ts.ts

--- back/src/config/typeorm.config.ts.ts
+++ back/src/config/typeorm.config.ts.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'sql202.infinityfree.com',
+  host: 'sql108.byethost17.com',
   port: 3306,
-  username: 'if0_39195419',
-  password: 'WCnsd8v2gHA30zJ', // Yeah this should definitely be in an env file but I don't care enough right now.
-  database: 'if0_39195419_gimnasio',
+  username: 'b17_39195534',
+  password: 'DB_PASSWORD',
+  database: 'b17_39195534_gimnasio',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true, // Set to false in production
 };

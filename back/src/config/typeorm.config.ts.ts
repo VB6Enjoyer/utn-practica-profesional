8e3f67e688adfe0c8f51c4c5974059470b1044df
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-06-09 22:20:35 -0300

    Updated database connection

--- back/src/config/typeorm.config.ts.ts
+++ back/src/config/typeorm.config.ts.ts
@@ -6,7 +6,7 @@ const config: TypeOrmModuleOptions = {
   port: 3306,
   username: 'if0_39195419',
   password: 'WCnsd8v2gHA30zJ', // Yeah this should definitely be in an env file but I don't care enough right now.
-  database: 'if0_39195419_gimnasio ',
+  database: 'if0_39195419_gimnasio',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true, // Set to false in production
 };
